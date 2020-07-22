4041015772607fc48ef96501a88507febfea56ed
YuriFaraf <yura96@gmail.com>
2020-07-23 01:11:15 +0500

    games controller anon tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -22,6 +22,43 @@ RSpec.describe GamesController, type: :controller do
       # Во flash должно быть сообщение об ошибке
       expect(flash[:alert]).to be
     end
+
+    it 'kicks from #create' do
+      # Вызываем экшен
+      post :create
+      # Проверяем ответ
+      # статус ответа не равен 200
+      expect(response.status).not_to eq(200)
+      # Devise должен отправить на логин
+      expect(response).to redirect_to(new_user_session_path)
+      # Во flash должно быть сообщение об ошибке
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicks from #answer' do
+      # Вызываем экшен
+      put :answer, id: game_w_questions.id, letter: 'a'
+      # Проверяем ответ
+      # статус ответа не равен 200
+      expect(response.status).not_to eq(200)
+      # Devise должен отправить на логин
+      expect(response).to redirect_to(new_user_session_path)
+      # Во flash должно быть сообщение об ошибке
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicks from #take_money' do
+      # Вызываем экшен
+      put :take_money, id: game_w_questions.id
+      # Проверяем ответ
+      # статус ответа не равен 200
+      expect(response.status).not_to eq(200)
+      # Devise должен отправить на логин
+      expect(response).to redirect_to(new_user_session_path)
+      # Во flash должно быть сообщение об ошибке
+      expect(flash[:alert]).to be
+    end
+
   end
 
   context 'Usual user' do
@@ -49,6 +86,22 @@ RSpec.describe GamesController, type: :controller do
       expect(flash[:notice]).to be
     end
 
+    # юзер пытается создать новую игру, не закончив старую
+    it 'try to create second game' do
+      # убедились что есть игра в работе
+      expect(game_w_questions.finished?).to be_falsey
+
+      # отправляем запрос на создание, убеждаемся что новых Game не создалось
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game) # вытаскиваем из контроллера поле @game
+      expect(game).to be_nil
+
+      # и редирект на страницу старой игры
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
+
     it '#show game' do
       # Показываем по GET-запросу
       get :show, id: game_w_questions.id
@@ -65,6 +118,19 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to render_template('show')
     end
 
+    # проверка, что пользовтеля посылают из чужой игры
+    it '#show alien game' do
+      # создаем новую игру, юзер не прописан, будет создан фабрикой новый
+      alien_game = FactoryBot.create(:game_with_questions)
+
+      # пробуем зайти на эту игру текущий залогиненным user
+      get :show, id: alien_game.id
+
+      expect(response.status).not_to eq(200) # статус не 200 ОК
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be # во flash должен быть прописана ошибка
+    end
+
     it 'answers correct' do
       # Дёргаем экшен answer, передаем параметр params[:letter]
       put :answer, id: game_w_questions.id, letter: game_w_questions.current_game_question.correct_answer_key
@@ -79,6 +145,24 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game))
       # Флеш пустой
       expect(flash.empty?).to be_truthy
+    end
+
+    # юзер берет деньги
+    it 'takes money' do
+      # вручную поднимем уровень вопроса до выигрыша 200
+      game_w_questions.update_attribute(:current_level, 2)
+
+      put :take_money, id: game_w_questions.id
+      game = assigns(:game)
+      expect(game.finished?).to be_truthy
+      expect(game.prize).to eq(200)
+
+      # пользователь изменился в базе, надо в коде перезагрузить!
+      user.reload
+      expect(user.balance).to eq(200)
+
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:warning]).to be
     end
   end
 end
